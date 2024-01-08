feb2f083ba1e426d06b662bff5743c0c671eb58e
Anna Nayara Lira <anna_nayara@hotmail.com>
2024-01-08 15:53:31 -0300

    criação do arquivo data/words

---
+++ src/data/words.ts
@@ -0,0 +1,17 @@
+export type WordsListProps = {
+    carro: string[];
+    fruta: string[];
+    corpo: string[];
+    computador: string[];
+    programação: string[];
+    alimento: string[];
+  };
+
+export const wordsList: WordsListProps = {
+  carro: ["Motor", "Porta", "Capô", "Pneu", "Antena"],
+  fruta: ["Banana", "Maçã", "Pêra", "Mamão", "Laranja"],
+  corpo: ["Braço", "Perna", "Cérebro", "Pescoço", "Olhos"],
+  computador: ["Mouse", "Teclado", "Monitor", "Gabinete"],
+  programação: ["Linguagem", "Framework", "JavaScript", "React"],
+  alimento: ["Arroz", "Feijão", "Carne", "Leite", "Ovo"],
+};
