09f89496ab7204e4da08d0654c3907cc7759fbe3
Anna Nayara Lira <anna_nayara@hotmail.com>
2024-02-19 09:54:16 -0300

    ajuste cor do texto

--- src/components/StartScreen/index.tsx
+++ src/components/StartScreen/index.tsx
@@ -5,7 +5,7 @@ interface StartScreenProps {
 
 export default function StartScreen({ startGame }: StartScreenProps) {
   return (
-    <div className="h-full flex flex-col gap-20">
+    <div className="h-full flex flex-col gap-20 text-white">
       <h2 className="text-2xl">Clique no botão abaixo para começar a jogar</h2>
       <Button buttonType="primary" onClick={startGame}>
         Iniciar o jogo
