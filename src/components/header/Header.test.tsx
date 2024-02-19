5cecc834051627504291dd4db63f264a791b522a
Anna Nayara Lira <anna_nayara@hotmail.com>
2024-02-19 09:59:13 -0300

    ajuste de caminho

--- src/components/header/Header.test.tsx
+++ src/components/header/Header.test.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
-import Header from './';
+import Header from '.';
 
 describe('Header Component', () => {
   test('renders correctly', () => {
